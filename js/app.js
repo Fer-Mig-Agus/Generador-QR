2649216788b2ae3e5d68510944b45ec683f3d554
Fer-Mig-Agus <miguelfernandez.m.f.8998@gmail.com>
2023-01-31 20:10:42 -0300

    He terminado el proyecto

---
+++ js/app.js
@@ -0,0 +1,91 @@
+
+const qrInput = document.querySelector('#url-box');
+const generateBtn = document.querySelector('#btn-generar');
+const qrImg = document.querySelector('#QR-image');
+const boxImagenQR = document.querySelector('#img-box-qr');
+const descarga = document.querySelector('#btn-Descargar');
+const eliminar = document.querySelector('#btn-EliminarQR');
+const boxQR = document.querySelector('#qr-generado');
+const mensajeBox = document.querySelector('#mensaje-box');
+const formulario = document.querySelector('#form');
+img = document.querySelector("img");
+let preValue;
+
+generateBtn.addEventListener("click", () => {
+    limpiarHTML();
+    if (qrInput.value !== "") {
+        let qrValue = qrInput.value.trim();
+        console.log(qrValue);
+        if (!qrValue || preValue === qrValue) return;
+        preValue = qrValue;
+        generateBtn.disabled = true;
+        generateBtn.style.cursor = "not-allowed";
+        boxQR.style.display = "block";
+        qrImg.src = `https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=${qrValue}`;
+        qrImg.addEventListener("load", () => {
+            generateBtn.innerText = "Codigo QR Generado";
+        });
+
+    } else {
+        mostrarMensaje("Campo Vacio");
+        return;
+    }
+
+});
+
+qrInput.addEventListener("keyup", () => {
+    if (!qrInput.value.trim()) {
+        preValue = "";
+    }
+});
+
+function mostrarMensaje(mensaje) {
+    const mensajeError = document.createElement('p');
+    const errores = document.querySelectorAll('.parrafo-mensaje');
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('parrafo-mensaje', 'failed');
+    if (errores.length === 0) {
+        mensajeBox.style.display = "block";
+        mensajeBox.appendChild(mensajeError);
+    }
+    setTimeout(() => {
+        mensajeBox.style.display = "none";
+    }, 3000);
+}
+
+
+function limpiarHTML() {
+    while (mensajeBox.firstChild) {
+        mensajeBox.removeChild(mensajeBox.firstChild);
+    }
+
+}
+descarga.addEventListener("click", () => {
+
+    let imgPath = img.getAttribute("src");
+    let nombreArchivo = getFileName(imgPath);
+    saveAs(imgPath, nombreArchivo);
+
+    //Volver el boton a la normalidad
+    generateBtn.disabled = false;
+    generateBtn.style.cursor = "pointer";
+    boxQR.style.display = "none";
+    qrImg.src = "";
+    generateBtn.innerText = "Generar QR";
+    formulario.reset();
+
+})
+
+function getFileName(str) {
+    return str.substr(str.lastIndexOf('/') + 1)
+}
+
+eliminar.addEventListener('click', () => {
+    //Volver el boton a la normalidad
+    generateBtn.disabled = false;
+    generateBtn.style.cursor = "pointer";
+    boxQR.style.display = "none";
+    qrImg.src = "";
+    generateBtn.innerText = "Generar QR";
+    formulario.reset();
+});
